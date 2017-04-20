3c2a35bdec84f3c90b460faf6c541a3f9392743d
Justin Mimbs <justin.mimbs@gmail.com>
2017-04-19 22:20:17 -0400

    Update Screen to render wrapped objects

--- src/Screen.elm
+++ src/Screen.elm
@@ -6,6 +6,11 @@ import Svg.Attributes
 import Math.Vector3 as Vector3 exposing (Vec3)
 
 
+screenId : String
+screenId =
+    "screen"
+
+
 render : ( Float, Float ) -> List (List Vec3) -> Html a
 render ( width, height ) polylines =
     Svg.svg
@@ -16,9 +21,15 @@ render ( width, height ) polylines =
             []
             [ Svg.text "@import url(../app/style.css);"
             ]
+        , Svg.defs
+            []
+            [ Svg.g
+                [ Svg.Attributes.id screenId ]
+                (polylines |> List.map viewPolyline)
+            ]
         , Svg.g
             []
-            (polylines |> List.map viewPolyline)
+            (translations |> List.map (viewProjection width height))
         ]
 
 
@@ -40,3 +51,29 @@ polylineToString =
 vecToString : Vec3 -> String
 vecToString vec =
     toString (Vector3.getX vec) ++ "," ++ toString (Vector3.getY vec)
+
+
+
+-- screen projections (3 x 3)
+
+
+viewProjection : Float -> Float -> ( Float, Float ) -> Svg a
+viewProjection width height ( u, v ) =
+    Svg.svg
+        [ Svg.Attributes.width (width |> toString)
+        , Svg.Attributes.height (height |> toString)
+        , Svg.Attributes.viewBox ([ width * u, height * v, width, height ] |> List.map toString |> String.join " ")
+        ]
+        [ Svg.use [ Svg.Attributes.xlinkHref ("#" ++ screenId) ] []
+        ]
+
+
+translations : List ( Float, Float )
+translations =
+    List.map (,) [ 0, 1, -1 ]
+        |> apply [ 0, 1, -1 ]
+
+
+apply : List a -> List (a -> b) -> List b
+apply =
+    List.concatMap << (flip List.map)
